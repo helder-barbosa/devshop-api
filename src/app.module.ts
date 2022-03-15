7354be820a6c5db52bf2cb39fe23d913c98d238b
Helder Barbosa <helderbarbosa.dev@gmail.com>
2022-03-15 20:52:14 -0300

    #3 Add TypeORM with Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GraphQLModule } from '@nestjs/graphql'
 import { CategoryModule } from './category/category.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgres://postgres:Root314@localhost:5432/devshop',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql'
     }),
